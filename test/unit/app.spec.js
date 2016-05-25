87183f88f86c7cf64b10c738f83c43ce2991eb36
Pato66 <patrykhudy@gmail.com>
2016-05-25 10:17:59 +0200

    changes

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -20,5 +20,33 @@ describe('app', function ()
         it('should return single number', function () {
             expect(app.reverseNumber(5)).toBe(5);
         });
+        //-----------------------------------------------------------------
+
+        it('should return single number', function () {
+            expect(app.reverseNumber(8)).toBe(8);
+        });it('should return single number', function () {
+            expect(app.reverseNumber(2)).toBe(2);
+        });
+        it('should return single number', function () {
+            expect(app.reverseNumber(1)).toBe(1);
+        });
+        it('should not accept not number as an argument', function () {
+            expect(app.reverseNumber('koza')).toEqual(false);
+        });
+        it('should not accept not number as an argument', function () {
+            expect(app.reverseNumber([2,1,3])).toEqual(false);
+        });
+        it('should not accept not number as an argument', function () {
+            expect(app.reverseNumber(true)).toEqual(false);
+        });
+        it('should not accept not number as an argument', function () {
+            expect(app.reverseNumber(false)).toEqual(false);
+        });
+        it('should not return a string', function () {
+            expect(app.reverseNumber(6543)).not.toBe('3456');
+        });
+        it('should not return a string', function () {
+            expect(app.reverseNumber(876543)).not.toBe('345678');
+        });
     });
 });
