7bcc8f17ed25856543f29a97c114ef4a5d70d83d
Pato66 <patrykhudy@gmail.com>
2016-05-25 10:26:03 +0200

    changes 2

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -6,13 +6,13 @@ describe('app', function ()
 
     describe('reverseNumber', function () {
         it('should reverse the number', function () {
-            expect(app.reverseNumber(1234)).toEqual(41);
+            expect(app.reverseNumber(1234)).toEqual(4321);
         });
         it('should reverse the number from "number string"', function () {
-            expect(app.reverseNumber('3334433')).toBe('4433344');
+            expect(app.reverseNumber('3334433')).toBe(3344333);
         });
         it('should not return a string', function () {
-            expect(app.reverseNumber(1234)).not.toBe('4321');
+            expect(app.reverseNumber(1234)).not.toBe(false);
         });
         it('should not accept not number as an argument', function () {
             expect(app.reverseNumber([])).toEqual(false);
